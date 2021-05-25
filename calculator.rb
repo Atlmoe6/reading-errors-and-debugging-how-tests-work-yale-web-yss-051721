8f8e2876e929d360b82b0b39c38741315777f596
abstract-inheritance-c040 <doe.mahamud1@gmail.com>
2021-05-25 09:16:15 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,7 @@
 # Add your variables here
+first_number = 3
+second_number = 6
+sum = first_number + second_number
+difference = second_number - first_number
+product = second_number * first_number
+quotient = second_number / first_number
